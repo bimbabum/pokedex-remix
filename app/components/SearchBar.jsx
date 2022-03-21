81f537c0f8c6ad4f87214a14df84cdd3a6e0bf32
JM <jane.trangm@gmail.com>
2022-03-21 08:04:02 +0700

    remove clear search

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,14 +1,8 @@
 import { useSubmit} from 'remix'
-import {useState} from 'react'
-
 
 export default function SearchBar(){
-    const [search, setSearch] = useState('')
     const submit = useSubmit()
-    const handleClearSearch = () => {
-      setSearch('')
-      submit()
-    }
+
     return(
       <>
         <div className='font-primary basis-3/4'>
@@ -22,18 +16,12 @@ export default function SearchBar(){
                 placeholder='Search Pokemons'
                 name='search'
                 className='border border-1 rounded-xl relative w-full p-2 pl-9'
-                value={search}
-                onChange={(e)=>setSearch(e.target.value)}
                 />
                 <img 
                   src={require('../img/search_icon.svg')} 
                   alt=''
                   className='absolute top-0 py-2 px-2'
                 />
-                <span 
-                  className={`absolute right-0 px-3 py-2  z-10 text-gray-600 cursor-pointer ${search? 'inline' : 'hidden'}`}
-                  onClick={handleClearSearch}
-                >x</span>
             </form>
         </div>
       </>
